66bd7aefcbe000f46601334fc948bec80aafd79c
Ahmed Tahir Shekhani <ahmedtahir2000@gmail.com>
2023-01-16 02:56:01 +0500

    login and error handling done

--- frontend/src/components/auth/login.js
+++ frontend/src/components/auth/login.js
@@ -2,57 +2,67 @@ import { loginAuth } from "../../services/utils";
 import React from "react";
 import { useState } from "react";
 const Login = () => {
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		console.log(username, password);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorShow, setErrorShow] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-		// loginAuth(username, password)
-		// 	.then((res) => {
-		// 		console.log(res);
-		// 	})
-		// 	.catch((err) => {
-		// 		console.log(err);
-		// 	});
-	};
+    loginAuth(username, password)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        const errMsg = err.response.data.message;
+        setErrorShow(true);
+        setErrorMsg(errMsg);
+      });
+  };
 
-	return (
-		<div>
-			<form onSubmit={(e) => handleSubmit(e)}>
-				<div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text" id="basic-addon1">
-							Username
-						</span>
-					</div>
-					<input
-						type="text"
-						class="form-control username"
-						placeholder="Enter Username"
-						aria-label="Username"
-						aria-describedby="basic-addon1"
-						onChange={(e) => setUsername(e.target.value)}
-					/>
-				</div>
-				<div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text" id="basic-addon2">
-							Pasword
-						</span>
-					</div>
-					<input
-						type="password"
-						class="form-control password"
-						placeholder="Enter Password"
-						aria-describedby="basic-addon2"
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-				</div>
-				<input type="submit" value="Login" className="btn btn-primary" />
-			</form>
-		</div>
-	);
+  return (
+    <div>
+      {errorShow ? (
+        <div class="alert alert-danger input-group-text" role="alert">
+          {errorMsg}
+        </div>
+      ) : (
+        ""
+      )}
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">
+              Username
+            </span>
+          </div>
+          <input
+            type="text"
+            class="form-control username"
+            placeholder="Enter Username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon2">
+              Pasword
+            </span>
+          </div>
+          <input
+            type="password"
+            class="form-control password"
+            placeholder="Enter Password"
+            aria-describedby="basic-addon2"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <input type="submit" value="Login" className="btn btn-primary" />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
